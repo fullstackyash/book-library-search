c8f2331ea9fecc5167e8a4bd0f448092e096570f
Yash Chopra <yash.chopra@infobeans.com>
2022-09-18 19:20:52 +0530

    book library search bootsrap plugin and required files

---
+++ book-library-search.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Book Library Search
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     book-library-search
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Book_Library_Search
+ */
+
+// Your code starts here.
